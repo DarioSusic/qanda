005b4902ab12657d13576f515f485288c65e061c
DarioSusic <dario.susic@gmail.com>
2019-10-25 15:58:05 +0200

    added test routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+//const cors = require('cors');
 
 //require('dotenv').config();
 
-const app = express();
-connectDB();
-
-const port = process.env.PORT || 5000;
-
+/*
 app.use(cors());
 app.use(express.json());
+*/
+
+//Db connection
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
 
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
