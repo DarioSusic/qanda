7ceb893b7d192c02ccf5d73a8b967c27f8069096
DarioSusic <dario.susic@gmail.com>
2019-10-26 15:58:14 +0200

    Delete profile and user

--- backend/models/Profile.js
+++ backend/models/Profile.js
@@ -1,40 +1,43 @@
 const mongoose = require('mongoose');
 
-const ProfileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user'
-  },
-  website: {
-    type: String
-  },
-  bio: {
-    type: String
-  },
-  interests: {
-    type: [String]
-  },
-  social: {
-    youtube: {
+const ProfileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user'
+    },
+    website: {
       type: String
     },
-    twitter: {
+    bio: {
       type: String
     },
-    facebook: {
-      type: String
+    interests: {
+      type: [String]
     },
-    linkeding: {
-      type: String
+    social: {
+      youtube: {
+        type: String
+      },
+      twitter: {
+        type: String
+      },
+      facebook: {
+        type: String
+      },
+      linkedin: {
+        type: String
+      },
+      instagram: {
+        type: String
+      }
     },
-    instagram: {
-      type: String
+    date: {
+      type: Date,
+      default: Date.now
     }
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -30,11 +29,6 @@ router.get('/me', auth, async (req, res) => {
 // @desc     Create or update user profile
 // @access   Private
 router.post('/', auth, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
   const {
     website,
     bio,
@@ -72,7 +66,7 @@ router.post('/', auth, async (req, res) => {
       profile = await Profile.findOneAndUpdate(
         { user: req.user.id },
         { $set: profileFields },
-        { new: true }
+        { new: true, upsert: true }
       );
       return res.json(profile);
     }
@@ -86,4 +80,55 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user and posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    //Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
