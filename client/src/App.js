b07ebea0b4037bb35da2f9ea00c7d08de92d4454
DarioSusic <dario.susic@gmail.com>
2019-10-31 19:49:30 +0100

    Profile view

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -38,6 +39,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -45,12 +45,12 @@ export const getProfiles = () => async dispatch => {
 };
 
 //Get profile by ID
-export const getProfilesByID = userId => async dispatch => {
+export const getProfilesById = userId => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
 
     dispatch({
-      type: GET_PROFILES,
+      type: GET_PROFILE,
       payload: res.data
     });
   } catch (err) {
