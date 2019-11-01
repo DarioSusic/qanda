ccb6dc01817a4539e3dc70a0f9f314d634da12bb
DarioSusic <dario.susic@gmail.com>
2019-11-01 18:04:42 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 5000;
-//const cors = require('cors');
 
-//require('dotenv').config();
-
-/*
-app.use(cors());
-app.use(express.json());
-*/
-
-//Db connection
+// Connect Database
 connectDB();
 
-//Init Middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//test route
-app.get('/', (req, res) => res.send('API Running'));
-
-//Defining routes
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
